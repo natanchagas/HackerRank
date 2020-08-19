b0b6295e7f670dcdf9a0e95f76c55366bf31885d
natanchagas <natan.c.silva@uol.com.br>
2020-08-19 00:02:18 -0300

    first commit

---
+++ 30 Days of Code/Python/Day 1 - Data Types.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i1 = 1
+d1 = 1.0
+s1 = 'String'
+# Read and save an integer, double, and String to your variables.
+i1 = input()
+d1 = input()
+s1 = input()
+# Print the sum of both integer variables on a new line.
+print(i + int(i1))
+# Print the sum of the double variables on a new line.
+print(d + float(d1))
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + s1)
